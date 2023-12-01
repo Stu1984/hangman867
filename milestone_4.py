dc63151a30c93406335290df4c75df45681449f6
Stuart Blair <stuartblair84@gmail.com>
2023-12-01 10:44:11 +0000

    adding docstrings to milestone_4.py & milestone_5.py

--- milestone_4.py
+++ milestone_4.py
@@ -6,7 +6,7 @@ class Hangman:
 
     def __init__(self, word_list: list, num_lives = 5):
         '''Class constructor to instantiate objects
-        
+
         Arguments:
         word_list: List - list of words the game uses to pick random word for user to guess
         num_lives: int, default = 5. Number of lives the player starts with'''
@@ -21,8 +21,15 @@ class Hangman:
         self.guessed_letters_list = [] #Empty list which will contain all letters guessed by user
 
     def check_guess(self, guess):
-        '''Method called from inside the ask_for_input method. 
-        Used to check if the guessed letter is in the random word and what happens if it is and is not.'''
+        '''Method called from inside the ask_for_input method.
+
+        Used to check if the guessed letter is in the random word and what happens if it is and is not.
+        If letter is included, the for loop is used to replace '_' with the guessed letter and
+        the num_letters attribute is reduced by 1.
+        If letter is not included in the random word, num_lives is reduced by 1. Guessed letter is appended to guessed_letters_list
+
+        Arguments:
+        guess (str) - the letter provided by the user'''
         guess = guess.lower()
         self.word = self.word.lower()
         if guess in self.word:
@@ -42,6 +49,13 @@ class Hangman:
             # return self.num_lives
       
     def ask_for_input(self):
+        '''ask_for_input method is called within the play_game function
+        
+        ask_for_input method asks the user to input their guess, 
+        checks whether the input is valid and will ask the user 
+        to try again if not or if the letter has already been used.
+        
+        If the guess is valid and unique the check_guess method is called and we break out of the loop'''
         while True:
             guess = str(input('Guess a letter in the word: '))
 
@@ -51,7 +65,7 @@ class Hangman:
                 print('You already tried that letter')
                 print(f'Letters guessed so far {self.guessed_letters_list}')
             else:
-                self.check_guess(guess)
+                self.check_guess(guess) # If the guess is valid and unique check_guess method is called
                 self.guessed_letters_list.append(guess)
                 break
 
--- milestone_5.py
+++ milestone_5.py
@@ -1,22 +1,32 @@
-# %%
+
 from milestone_4 import Hangman
 
 # game1 = Hangman(['peach', 'apple', 'tomato'])
 # game1.ask_for_input()
 
 def play_game(word_list):
+    '''call this function to play the game
+    
+    Play game function instantiates the game object of the Hangman class. 
+    Using a while loop it checks the status of play, 
+    if the game has not yet been won or lost, 
+    it calls the ask_for_input method from Hangman class.
+    
+    Arguments:
+    word_list (List) - list of words that the Hangman class will use to select a random word'''
     num_lives = 5
     game = Hangman(word_list, num_lives)
     print('Welcome to Hangman')
     print('Try and guess the word')
     while True:
-        if game.num_lives == 0:
+        if game.num_lives == 0: #Checks if player has any lives left
             print('Sorry! You lost')
             break
-        elif game.num_letters == 0:
+        elif game.num_letters == 0: #Checks if player has already guessed all the letters
             print("Congrats! You're a winner")
             break
         else:
-            game.ask_for_input()
+            game.ask_for_input() 
    
-play_game(['peach', 'apple', 'tomato'])
+fruits = ['pear', 'apple', 'orange', 'plum', 'tangerine']
+play_game(fruits)
